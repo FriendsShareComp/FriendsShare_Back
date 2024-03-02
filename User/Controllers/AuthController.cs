2d69cea744b16cb499a060fd8fe6d4e480f4d1f2
Gianlu125 <g.pastore@ittl.com.ar>
2024-03-02 19:07:46 -0300

    correccion pathbase /User

---
+++ User/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Aplication.Interfaces;
+using Domain.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace User.Controllers
+{
+    [ApiController]
+    //[Route("/User")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService) 
+        {
+            _authService = authService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("Login")]
+        public IActionResult LoginUser(dtoLoginUser loginUser)
+        {
+            try
+            {
+                var ClientExist = _authService.GetLoginUser(loginUser);
+                if (!ClientExist.succes)
+                {
+                    return new JsonResult(new { Error = ClientExist.content }) { StatusCode = ClientExist.StatusCode };
+                }
+                return new JsonResult(new { Token = ClientExist.objects }) { StatusCode = ClientExist.StatusCode };
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+
+        }
+    }
+}
